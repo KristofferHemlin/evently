7ea92f864ce7c1a453f321adf528834e6cd6d82a
CLAMON-8244LRQ\Kristoffer Hemlin <kristoffer.hemlin@claremont.se>
2019-09-23 15:52:14 +0200

    test package

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,22 +7,22 @@ import {
     Text,
 } from 'react-native';
 
-// import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-// const info_circ = <FontAwesome5 size={25} name={'info-circle'} brand color="black" />;
-// const users = <FontAwesome5 size={25} name={'users'} brand color="black" />;
+const info_circ = <FontAwesome5 size={25} name={'info-circle'} brand color="black" />;
+const users = <FontAwesome5 size={25} name={'users'} brand color="black" />;
 
 const footer = props => {
     return (
         <View style={styles.footerContainer}>
             <TouchableOpacity style={styles.footerButton}>
-                {/* {info_circ} */}
+                {info_circ}
                 <Text>
                     Company info
                 </Text>
             </TouchableOpacity>
             <TouchableOpacity style={styles.footerButton}>
-                {/* {users} */}
+                {users}
                 <Text>
                     Employees
                 </Text>
