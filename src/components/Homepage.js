dbd09598bad3bfcc115285e943eedca286421b22
Tom Althin <talthin@Toms-MacBook-Pro.local>
2019-09-24 14:29:32 +0200

    changed homepage to look like before

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,34 +5,21 @@ import {
     ScrollView,
     Button
 } from 'react-native';
-import ImagePicker from 'react-native-image-picker';
 
 import Header from './Header';
 import Footer from './Footer';
 
 class Homepage extends Component{
-
-    handlePhotos = () => {
-        const options = {}; 
-        ImagePicker.launchImageLibrary(options, response => {
-            console.log("response", response);
-        });
-    };
     
     render(){
         return(
             <View style={styles.homepageContainer}>
-                <Button
-                title="Photo"
-                onPress={this.handlePhotos}/>
-            </View>
-            // <View style={styles.homepageContainer}>
-            //     <ScrollView style ={styles.bodyContainer}> 
+                <ScrollView style ={styles.bodyContainer}> 
                   
-            //     </ScrollView>
-            //     <Header/>
-            //     <Footer/>
-            // </View>
+                </ScrollView>
+                <Header/>
+                <Footer/>
+            </View>
             
         )
     }
