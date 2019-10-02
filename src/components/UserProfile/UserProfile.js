c1a3286a896be3f7394175bc5044d1e665b578be
Tom Althin <talthin@Toms-MacBook-Pro.local>
2019-10-02 09:15:40 +0200

    added keyboard awarescroll and key to map function

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -7,70 +7,74 @@ import {
     TextInput,
     TouchableOpacity,
 } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'; // Använde ett package då vanliga avoidkeybord inte funka
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import Croatia from '../EventOverview/images/CROT.jpg';
 import styles from './UserProfile.style.js';
 import HeadlineOverview from '../HeadlineOverview/HeadlineOverview';
 
-class UserProfile extends Component{
+import Croatia from '../EventOverview/images/CROT.jpg';
+
+
+
+class UserProfile extends Component {
 
     static navigationOptions = {
-        header : null,
-      };
+        header: null,
+    };
 
     state = {
-            name: 'Mr. Andersson',
-            // company: 'CNS/CAD hybrid',
-            // role: 'Company Bro', 
-            email: 'Andersson@claremont.se',
-            phone: '070 999 999',
-            about: 'nah bruh',
-            allergies: 'Ogillar papaya',
-            image: Croatia,
+        name: 'Mr. Andersson',
+        // company: 'CNS/CAD hybrid',
+        // role: 'Company Bro', 
+        email: 'Andersson@claremont.se',
+        phone: '070 999 999',
+        about: 'nah bruh',
+        allergies: 'Ogillar papaya',
+        image: Croatia,
 
-            isLoggedIn: true,
+        isLoggedIn: false,
 
 
-            fields: [
-                {
-                    key: 'firstName',
-                    name: 'First Name',
-                    type: 'text',
-                    label: 'First Name',
-                    value: '',
-                    secureText: false,      
-                },
-                {
-                    key: 'lastName',
-                    name: 'Last Name',
-                    type: 'text',
-                    label: 'Last Name',
-                    value: '',
-                    secureText: false,
-                  },
-                  {
-                    key: 'email',
-                    name: 'Email',
-                    type: 'text',
-                    label: 'Email',
-                    value: '',
-                    secureText: false,
-                  },
-                  {
-                    key: 'phone',
-                    name: 'Phone',
-                    type: 'text',
-                    value: '',
-                    secureText: false,
-                  },
-            ]
-        }
+        fields: [
+            {
+                key: 'firstName',
+                name: 'First Name',
+                type: 'text',
+                label: 'First Name',
+                value: '',
+                secureText: false,
+            },
+            {
+                key: 'lastName',
+                name: 'Last Name',
+                type: 'text',
+                label: 'Last Name',
+                value: '',
+                secureText: false,
+            },
+            {
+                key: 'email',
+                name: 'Email',
+                type: 'text',
+                label: 'Email',
+                value: '',
+                secureText: false,
+            },
+            {
+                key: 'phone',
+                name: 'Phone',
+                type: 'text',
+                value: '',
+                secureText: false,
+            },
+        ]
+    }
 
-    
 
-    render(){
+
+    render() {
 
         const isLoggedIn = this.state.isLoggedIn;
 
@@ -79,35 +83,33 @@ class UserProfile extends Component{
         let inputInformation;
         if (isLoggedIn) {
             inputInformation = <View style={styles.inputForm}>
-                            
-                            {this.state.fields.map((input, idx) => {
-                                return (
-                                    <View>
-                                        <Text style={styles.inputFormTitle}>{input.name}</Text>
-                                        <TextInput
-                                        value={input.value}
-                                        style={styles.input}
-                                        name={input.name}
-                                        key={input.key}
-                                        type={input.type}
-                                        label={input.label}
-                                        placeholder={input.name}
-                                        secureTextEntry={input.secureText}
-                                        onChangeText={(value) => this.handleInputChange(value, idx)}
-                                        />
-                                    </View>
-                                )
-                                
-                                })}
-                            <TouchableOpacity   
-                                style={styles.buttonContainer}
-                                onPress={this.handleSubmit}>
-                            {this.state.isLoading ? <ActivityIndicator size='small' color='white'/> : <Text style = {styles.buttonText}>Submit </Text>} 
-                            </TouchableOpacity>
-                         </View>;
+                {this.state.fields.map((input, idx) => {
+                    return (
+                        <View key={input.key}>
+                            <Text style={styles.inputFormTitle}>{input.name}</Text>
+                            <TextInput
+                                value={input.value}
+                                style={styles.input}
+                                name={input.name}
+                                type={input.type}
+                                label={input.label}
+                                placeholder={input.name}
+                                secureTextEntry={input.secureText}
+                                onChangeText={(value) => this.handleInputChange(value, idx)}
+                            />
+                        </View>
+                    )
+
+                })}
+                <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={this.handleSubmit}>
+                    {this.state.isLoading ? <ActivityIndicator size='small' color='white' /> : <Text style={styles.buttonText}>Submit </Text>}
+                </TouchableOpacity>
+            </View>;
         }
 
-        else{
+        else {
             console.log("Render nothing");
             inputInformation = <View>
                 {/* <Text style={styles.subTitles}>Role</Text>
@@ -123,29 +125,31 @@ class UserProfile extends Component{
             </View>;
         }
 
-        return(
+        return (
             <View style={styles.pageContainer}>
-                <Header/>
+                <Header />
 
                 <ScrollView>
-                    <View style={styles.userInfo}>
+                    <KeyboardAwareScrollView>
+                        <View style={styles.userInfo}>
 
-                        <HeadlineOverview infoButtonStatus={false} editButtonStatus={isLoggedIn}>User Profile</HeadlineOverview>
+                            <HeadlineOverview infoButtonStatus={false} editButtonStatus={isLoggedIn}>User Profile</HeadlineOverview>
 
-                        <View style={styles.profilePictureView}>
-                            <Image source={this.state.image} style={styles.profilePicture}/>
-                            <Text style={styles.nameText}>{this.state.name}</Text>
-                            {/* <Text style={styles.companyText}>{this.state.company}</Text>         */}
+                            <View style={styles.profilePictureView}>
+                                <Image source={this.state.image} style={styles.profilePicture} />
+                                <Text style={styles.nameText}>{this.state.name}</Text>
+                                {/* <Text style={styles.companyText}>{this.state.company}</Text>         */}
+                            </View>
+
+                            <View style={styles.line}></View>
+
+                            {inputInformation}
                         </View>
+                    </KeyboardAwareScrollView>
 
-                        <View style={styles.line}></View>
-
-                        {inputInformation}
-
-                    </View>
                 </ScrollView>
-                
-                <Footer/>
+
+                <Footer />
             </View>
         )
     }
