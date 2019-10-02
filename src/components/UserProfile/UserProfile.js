3cf63dc0738bde92347ae5f6a0feaf0ad44d4be0
Tom Althin <talthin@Toms-MacBook-Pro.local>
2019-10-02 14:27:16 +0200

    split userprofile into two files

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -4,14 +4,13 @@ import {
     Text,
     ScrollView,
     Image,
-    TextInput,
-    TouchableOpacity,
 } from 'react-native';
+import axios from 'axios';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'; // Använde ett package då vanliga avoidkeybord inte funka
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
-import styles from './UserProfile.style.js';
+import styles from './UserProfile.style';
 import HeadlineOverview from '../HeadlineOverview/HeadlineOverview';
 
 import Croatia from '../EventImageHeader/images/CROT.jpg';
@@ -25,130 +24,74 @@ class UserProfile extends Component {
     };
 
     state = {
-        name: 'Mr. Andersson',
-        // company: 'CNS/CAD hybrid',
-        // role: 'Company Bro', 
-        email: 'Andersson@claremont.se',
+
+        firstName: '',
+        lastName: '',
+        // company: '',
+        // role: '', 
+        email: '',
         phone: '070 999 999',
         about: 'nah bruh',
         allergies: 'Ogillar papaya',
         image: Croatia,
 
-        isLoggedIn: false,
+        ownProfilePage: true,
 
 
-        fields: [
-            {
-                key: 'firstName',
-                name: 'First Name',
-                type: 'text',
-                label: 'First Name',
-                value: '',
-                secureText: false,
-            },
-            {
-                key: 'lastName',
-                name: 'Last Name',
-                type: 'text',
-                label: 'Last Name',
-                value: '',
-                secureText: false,
-            },
-            {
-                key: 'email',
-                name: 'Email',
-                type: 'text',
-                label: 'Email',
-                value: '',
-                secureText: false,
-            },
-            {
-                key: 'phone',
-                name: 'Phone',
-                type: 'text',
-                value: '',
-                secureText: false,
-            },
-        ]
     }
 
-
+    componentDidMount() {
+        // uID = Number(this.props.navigation.getParam('uID', '')) // kan finnas bättre ställe att hämta params?
+        axios.get('http://localhost:3000/users/109')
+            .then((response) => {
+                this.setState({
+                    firstName: response.data.firstName,
+                    lastName: response.data.lastName,
+                    email: response.data.email,
+                    phone: response.data.phone
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
     render() {
-
-        const isLoggedIn = this.state.isLoggedIn;
-
-        console.log("testing: ", this.state.fields[3].type);
-
-        let inputInformation;
-        if (isLoggedIn) {
-            inputInformation = <View style={styles.inputForm}>
-                {this.state.fields.map((input, idx) => {
-                    return (
-                        <View key={input.key}>
-                            <Text style={styles.inputFormTitle}>{input.name}</Text>
-                            <TextInput
-                                value={input.value}
-                                style={styles.input}
-                                name={input.name}
-                                type={input.type}
-                                label={input.label}
-                                placeholder={input.name}
-                                secureTextEntry={input.secureText}
-                                onChangeText={(value) => this.handleInputChange(value, idx)}
-                            />
-                        </View>
-                    )
-
-                })}
-                <TouchableOpacity
-                    style={styles.buttonContainer}
-                    onPress={this.handleSubmit}>
-                    {this.state.isLoading ? <ActivityIndicator size='small' color='white' /> : <Text style={styles.buttonText}>Submit </Text>}
-                </TouchableOpacity>
-            </View>;
-        }
-
-        else {
-            console.log("Render nothing");
-            inputInformation = <View>
-                {/* <Text style={styles.subTitles}>Role</Text>
-                <Text style={styles.ordinaryText}>{this.state.role}</Text> */}
-                <Text style={styles.subTitles}>Email</Text>
-                <Text style={styles.ordinaryText}>{this.state.email}</Text>
-                <Text style={styles.subTitles}>Phone</Text>
-                <Text style={styles.ordinaryText}>{this.state.phone}</Text>
-                <Text style={styles.subTitles}>About</Text>
-                <Text style={styles.ordinaryText}>{this.state.about}</Text>
-                <Text style={styles.subTitles}>Allergies</Text>
-                <Text style={styles.ordinaryText}>{this.state.allergies}</Text>
-            </View>;
-        }
-
         return (
             <View style={styles.pageContainer}>
                 <Header />
-
                 <ScrollView>
                     <KeyboardAwareScrollView>
                         <View style={styles.userInfo}>
 
-                            <HeadlineOverview infoButtonStatus={false} editButtonStatus={isLoggedIn}>User Profile</HeadlineOverview>
+                            <HeadlineOverview
+                                infoButtonStatus={false}
+                                editButtonStatus={true}
+                            >User Profile</HeadlineOverview>
 
                             <View style={styles.profilePictureView}>
                                 <Image source={this.state.image} style={styles.profilePicture} />
-                                <Text style={styles.nameText}>{this.state.name}</Text>
+                                <Text style={styles.nameText}>{this.state.firstName} {this.state.lastName}</Text>
                                 {/* <Text style={styles.companyText}>{this.state.company}</Text>         */}
                             </View>
 
                             <View style={styles.line}></View>
 
-                            {inputInformation}
+                            <View>
+                                {/* <Text style={styles.subTitles}>Role</Text>
+                <Text style={styles.ordinaryText}>{this.state.role}</Text> */}
+                                <Text style={styles.subTitles}>Email</Text>
+                                <Text style={styles.ordinaryText}>{this.state.email}</Text>
+                                <Text style={styles.subTitles}>Phone</Text>
+                                <Text style={styles.ordinaryText}>{this.state.phone}</Text>
+                                <Text style={styles.subTitles}>About</Text>
+                                <Text style={styles.ordinaryText}>{this.state.about}</Text>
+                                <Text style={styles.subTitles}>Allergies</Text>
+                                <Text style={styles.ordinaryText}>{this.state.allergies}</Text>
+                            </View>
                         </View>
                     </KeyboardAwareScrollView>
-
                 </ScrollView>
-
                 <Footer />
             </View>
         )
