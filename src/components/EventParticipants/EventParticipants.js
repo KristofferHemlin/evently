08a2ff9d70c3f978162a6b501096f8f46c767087
Marcus Hogler <marcus@Marcuss-MacBook-Pro-Claremont.local>
2019-10-04 10:54:54 +0200

    The users' names are added from the database to the slightly updated  ProfilePreview component

--- src/components/EventParticipants/EventParticipants.js
+++ src/components/EventParticipants/EventParticipants.js
@@ -6,6 +6,8 @@ import {
     TextInput,
 } from 'react-native';
 
+import axios from 'axios';
+
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import ProfilePreview from '../ProfilePreview/ProfilePreview';
@@ -21,13 +23,8 @@ class EventParticipants extends Component{
       };
 
       state = {
-        eventTitle: '',
-        eventId: null,
-        eventLocation: '', 
-        eventDesc: '',
-        startTime: '',
-        endTime: '',
-        niceToKnow: '',
+        firstName: '',
+        lastName: '', 
         uID: null,
 
         filterWord: '',
@@ -67,30 +64,51 @@ class EventParticipants extends Component{
             }
         ]
     }
+
+    //Tommygun
+    componentDidMount () {
+        uID = Number(this.props.navigation.getParam('uID', ''));
+        // axios.get('http://localhost:3000/users/' + uID + '/currentevent')
+        axios.get('http://localhost:3000/events/1/users')
+        .then((response) => {
+
+        console.log(response);
+        // console.log(response.data[1].firstName);
+        // console.log(response.data[1].lastName);
+        // console.log(response.data[1].id);
+        // console.log(response.data[1].profileImageUrl);
+
+          this.setState({
+              firstName: response.data[0].firstName,
+              lastName: response.data[0].lastName,
+              uID: response.data[0].id,
+            }
+          )
+        })
+        .catch((error) => {
+            console.log(error);
+        });     
+    }
+    //Tommygun
  
     render(){
 
         const filterWord = this.state.filterWord;
-        console.log("filterWord: ", filterWord);
-
-        // console.log(this.state.profileArray.filter, this.state.profileArray,{ name: 'Marcus' });
-        console.log(this.state.profileArray);
+        // console.log("filterWord: ", filterWord);
 
         this.state.profileArray = this.state.profileArray.filter(function(person) {
             // return person.name == 'Marcus';
-
             return person.name.includes(filterWord);
-            // return person.name == 'Marcus';
         });
 
-        console.log(this.state.profileArray);
+        // console.log(this.state.profileArray);
 
         return(
             <View style={styles.pageContainer}>
                 <Header/>
                 <ScrollView>
                     <EventImageHeader/>
-                    <HeadlineOverview infoButtonStatus={true} editButtonStatus={true}>Event Preview</HeadlineOverview>
+                    <HeadlineOverview infoButtonStatus={true} editButtonStatus={true}>Event Participants</HeadlineOverview>
 
                     <TextInput style={styles.searchBar}
                         placeholder = "Search current event participants ..."
@@ -104,8 +122,8 @@ class EventParticipants extends Component{
 
 
                     <View style={styles.profileList}>
-                        {this.state.profileArray.map(() => {
-                            return <ProfilePreview/>
+                        {this.state.profileArray.map((input, index) => {
+                            return <ProfilePreview key={index}>{this.state.firstName} {this.state.lastName}</ProfilePreview>
                             })}
                     </View>
 
--- src/components/ProfilePreview/ProfilePreview.js
+++ src/components/ProfilePreview/ProfilePreview.js
@@ -10,27 +10,31 @@ import Croatia from '../EventImageHeader/images/CROT.jpg';
 import leftPad from 'left-pad';
 import styles from './ProfilePreview.style.js'
 
-class ProfilePreview extends Component{
+// class ProfilePreview extends Component{
+
+const profilePreview = props => {
 
     state = {
-        name: 'Marcus Nilsson', 
+        // name: 'Marcus Nilsson', 
+        // name: '',
+        // surname: '', 
         title: 'Company manager',
         profilePicture: Croatia,
     }
 
-    render(){
-        return(
-            <TouchableOpacity>
-                <View style={styles.profileOverviewView}>
-                    <Image source={this.state.profilePicture} style={styles.previewImg}></Image>
-                    <View style={styles.profileOverviewViewRight}>
-                        <Text style={styles.textName}>{this.state.name}</Text>
-                        <Text style={styles.textTitle}>{this.state.title}</Text>
-                    </View>
+    return(
+        <TouchableOpacity>
+            <View style={styles.profileOverviewView}>
+                <Image source={this.state.profilePicture} style={styles.previewImg}></Image>
+                <View style={styles.profileOverviewViewRight}>
+                    {/* <Text style={styles.textName}>{this.state.name}</Text> */}
+                    <Text style={styles.textName}>{props.children}</Text>
+                    {/* <Text style={styles.textTitle}>{this.state.title}</Text> */}
+                    <Text style={styles.textTitle}>{this.state.title}</Text>
                 </View>
-            </TouchableOpacity>
-        )
-    }
-}
+            </View>
+        </TouchableOpacity>
+    );
+};
 
-export default ProfilePreview;
+export default profilePreview;
