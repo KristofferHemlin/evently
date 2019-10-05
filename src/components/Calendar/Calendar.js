99d922e12c40a7219033c79a6d2ef1c62fca93de
Tom Althin <talthin@Toms-MacBook-Pro.local>
2019-10-05 14:55:09 +0200

    added isUpdated variable to caledar to get it to load right

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Dimensions, View, ScrollView } from 'react-native';
 import axios from 'axios';
 import EventCalendar from 'react-native-events-calendar'
+import moment from 'moment';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
@@ -23,23 +24,23 @@ class Calendar extends Component {
       // { start: '2019-09-27 09:10:00', end: '2019-09-27 11:40:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032 ' },
       // { start: '2019-09-27 08:10:00', end: '2019-09-27 08:45:00', title: 'Dr. Mariana Joseph', summary: '3412 Piedmont Rd NE, GA 3032,' },
     ],
+    isUpdated: false,
   }
 
   componentDidMount() {
-    axios.get('http://localhost:3000/events/1/activities')
-      // axios.put('http://10.100.134.115:3000/users/' + this.state.uID + '/firstlogin', {
-      // moment.js
+    // axios.get('http://localhost:3000/events/1/activities')
+    axios.get('http://192.168.0.100:3000/events/1/activities')
       .then((response) => {
         console.log(response.data)
-        responseArray = response.data.map(activity =>({
-          start: activity.startTime,
-          end:  activity.endTime,
+        responseArray = response.data.map(activity => ({
+          start: moment(new Date(activity.startTime.replace(' ', 'T'))).format('YYYY-MM-DD HH:mm:ss'),
+          end: moment(new Date(activity.endTime.replace(' ', 'T'))).format('YYYY-MM-DD HH:mm:ss'),
           title: activity.title,
           summary: activity.description
         })
         )
 
-        this.setState({activities: responseArray},() => console.log("denna", this.state.activities))
+        this.setState({ activities: responseArray, isUpdated: true }, () => console.log("denna", this.state.activities))
         // this.setState({activities: respsoneMap},() => console.log("denna", this.state.activities))
 
       })
@@ -58,7 +59,8 @@ class Calendar extends Component {
         <Header />
         <HeadlineOverview infoButtonStatus={true} editButtonStatus={true}>Schedule</HeadlineOverview>
         <View style={styles.calendarContainer}>
-          <EventCalendar
+          {this.state.isUpdated ?
+           <EventCalendar
             format24h
             eventTapped={this.eventClicked.bind(this)}
             //Function on event press
@@ -69,11 +71,11 @@ class Calendar extends Component {
             size={30}
             //number of date will render before and after initDate 
             //(default is 30 will render 30 day before initDate and 29 day after initDate)
-            initDate={'2019-09-27'}
+            initDate={'2019-10-19'}
             //show initial date (default is today)
             scrollToFirst
           //scroll to first event of the day (default true)
-          />
+          /> : null}
         </View>
         <Footer />
       </View>
