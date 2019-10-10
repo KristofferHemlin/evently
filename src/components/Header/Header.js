c3fe37b5894d49c7b963bedcb4988d77fec27a06
Marcus Hogler <marcus@Marcuss-MacBook-Pro-Claremont.local>
2019-10-10 15:05:41 +0200

    red circle added to bell

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,9 +14,9 @@ import bgImage from './images/login-bg.png'; // TODO: Hitta bättre bild??
 
 
 const header = props => {
-    let bellColor = '';
-    {props.showNotificationBadge ? bellColor='red' : bellColor='white'}
-    const bell_icon = <FontAwesome5 size={25} name={'bell'} light color={bellColor} />; // best pratice att lägga en const utanför huvudfunktionen i react?? 
+    // let bellColor = '';
+    // {props.showNotificationBadge ? bellColor='red' : bellColor='white'}
+    const bell_icon = <FontAwesome5 size={25} name={'bell'} light color={"white"} />; // best pratice att lägga en const utanför huvudfunktionen i react?? 
     const user_cog = <FontAwesome5 size={25} name={'user-cog'} light color="white" />;
     return (
         <ImageBackground source={bgImage} style={styles.headerContainer}>
@@ -26,10 +26,12 @@ const header = props => {
                 {/*TODO: borde inte vara hårdkodat */}
             </View>
             <View style={styles.iconContainer}>
-                <TouchableOpacity 
-                style={styles.notificationIcon}
-                onPress={props.bellIconClicked}>
-                    {bell_icon}
+                <TouchableOpacity onPress={props.bellIconClicked}>
+                    <View style={styles.notificationIcon}>
+                        {props.showNotificationBadge ?
+                            <View style={styles.notificationIconCircle}/>: null}
+                        {bell_icon}
+                    </View>
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={styles.profileIcon}
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -21,9 +21,18 @@ export default StyleSheet.create({
     },
     notificationIcon:{
         marginRight: 30,
+        alignContent: 'flex-end',
+    },
+    notificationIconCircle:{
+        width: 10,
+        height: 10,
+        borderRadius: 10/2,
+        backgroundColor: 'red',
+        alignSelf: 'flex-end',
+        position: 'absolute',
+        zIndex: 1,
     },
     profileIcon:{
         marginRight: 20,
-
-    }
+    },
 });
