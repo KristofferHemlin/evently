8b4af37f13769ae40893ad14945e63dfffdb60be
Tom Althin <talthin@Toms-MacBook-Pro.local>
2019-10-10 14:24:36 +0200

    fixed fontweight login

--- src/components/Login/Login.style.js
+++ src/components/Login/Login.style.js
@@ -15,7 +15,7 @@ export default StyleSheet.create({
     logoText:{
         color: "white",
         fontSize: 40,
-        fontWeight: '200',
+        fontWeight: '300',
     },
 
     signUpContainer:{
@@ -28,7 +28,7 @@ export default StyleSheet.create({
     signText:{
         color: "white",
         fontSize: 20,
-        fontWeight: '200',
+        fontWeight: '400',
         alignSelf:'center',
     }
 });
