0680951ff50829088511ddcc6903cf397e885792
Tom Althin <talthin@Toms-MacBook-Pro.local>
2019-10-14 16:39:09 +0200

    changeinfo can now handle change password requests

--- src/components/ChangeInfo/ChangeInfo.js
+++ src/components/ChangeInfo/ChangeInfo.js
@@ -24,13 +24,13 @@ class ChangeInfo extends Component {
     };
 
     state = {
-            title: this.props.navigation.getParam('title', ''),
-            parentRoute: this.props.navigation.getParam('parentRoute', ''),
-            http_update_url: this.props.navigation.getParam('http_update_url', ''),
-            fields: this.props.navigation.getParam('fields', ''),
-            isLoading: false,
-            wantToEdit: false,
-            uID: this.props.navigation.getParam('uID', '')
+        title: this.props.navigation.getParam('title', ''),
+        parentRoute: this.props.navigation.getParam('parentRoute', ''),
+        http_update_url: this.props.navigation.getParam('http_update_url', ''),
+        fields: this.props.navigation.getParam('fields', ''),
+        isLoading: false,
+        wantToEdit: false,
+        uID: this.props.navigation.getParam('uID', '')
     }
 
     handleInputChange = (value, key) => {
@@ -40,16 +40,23 @@ class ChangeInfo extends Component {
     };
 
     handleSubmit = () => {
-        var fields = Object.keys(this.state.fields).reduce((map, key) => {
+        var body = Object.keys(this.state.fields).reduce((map, key) => {
             map[key] = this.state.fields[key].value
             return map
         }, {})
-        fields.title = this.state.title
+        body.title = this.state.title
+        body.token = this.state.token || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyNiwiY29tcGFueV9pZCI6MSwicm9sZSI6eyJpZCI6MSwiY3JlYXRlZEF0IjoiMjAxOS0xMC0wMVQxMToyMzoxNS44MzBaIiwidXBkYXRlZEF0IjoiMjAxOS0xMC0wMVQxMToyMzoxNS44MzBaIiwicm9sZSI6IkNPTVBBTllfTUFOQUdFUiJ9LCJpYXQiOjE1NzEwNjI3OTEsImV4cCI6MTU3MTE0OTE5MX0.O0T6zCJ_GxURvdVL8qInldh7FRQh6gfAEAPIq1rJ-0U"
 
         this.setState({ isLoading: true }, () => {
-            axios.put(this.state.http_update_url, fields)
-                .then(() => this.props.navigation.state.params.onEditSubmit(fields))
-                .then(() => this.props.navigation.navigate(this.state.parentRoute))
+            axios.put(this.state.http_update_url, body)
+                .then(() => this.props.navigation.state.params.onEditSubmit(body))
+                .then(() =>
+                    this.setState({ isLoading: false }, () => {
+                        this.props.navigation.navigate(this.state.parentRoute, {
+                            uID: this.state.uID
+                        })
+                    })
+                )
                 .catch((error) => {
                     console.log(error);
                     this.setState({ isLoading: false })
@@ -61,7 +68,7 @@ class ChangeInfo extends Component {
         return (
             !this.state ? <View /> :
                 <View style={styles.pageContainer}>
-                    <View  style={{height: 40}}/>
+                    <View style={{ height: 40 }} />
                     <ScrollView>
                         <KeyboardAwareScrollView>
                             <View style={styles.userInfo}>
@@ -77,7 +84,7 @@ class ChangeInfo extends Component {
                                     isLoading={this.state.isLoading}
                                     handleInputChange={this.handleInputChange}
                                     formStyle={styles} />
-                                    
+
                             </View>
                         </KeyboardAwareScrollView>
                     </ScrollView>
