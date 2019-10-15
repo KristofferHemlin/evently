cbc40106a3914629de6dcc861e572e1e5b055c77
Tom Althin <talthin@Toms-MacBook-Pro.local>
2019-10-15 17:11:28 +0200

    added updatedAt to notificationOVerview

--- src/components/NotificationModal/NotificationModal.js
+++ src/components/NotificationModal/NotificationModal.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
 import { withNavigation } from 'react-navigation';
 import { NavigationEvents } from 'react-navigation';
-
+import moment from 'moment';
 import axios from 'axios';
 
 
@@ -17,58 +17,60 @@ class NotificationOverview extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            notifications: []  
+            notifications: []
         }
-        props.navigation.addListener('didFocus', () => {
-            
-        })
-
         this.fetchNotifications()
     }
 
 
 
     fetchNotifications() {
-        console.log('agajagjasdg',this.props.uID)
-        axios.get(SERVER_ENDPOINT + '/users/' + this.props.uID + '/notifications')
+        console.log('agajagjasdg', this.props.uID)
+        // axios.get(SERVER_ENDPOINT + '/users/' + this.props.uID + '/notifications')
+        axios.get(SERVER_ENDPOINT + '/users/26/notifications')
             .then((results) => {
                 console.log(results)
-                var filteredRes = (results.data || []).reduce((map, {activity: {title, id, updatedAt}}) => {
-                    map[id] = {title, id, updatedAt, routeType: 'ActivityOverviewRoute'}
+                var filteredRes = (results.data || []).reduce((map, { activity: { title, id, updatedAt } }) => {
+                    map[id] = {
+                        title,
+                        id,
+                        updatedAt: moment(new Date(updatedAt.replace(' ', 'T'))).format('YYYY-MM-DD HH:mm'),
+                        routeType: 'ActivityOverviewRoute'
+                    }
                     return map
                 }, {})
-                this.setState({notifications: Object.keys(filteredRes).map(key => filteredRes[key])}); 
-            })   
+                this.setState({ notifications: Object.keys(filteredRes).map(key => filteredRes[key]) });
+            })
 
     }
 
     navigateTo(routeType, itemID) {
-        this.setState({notifications: []})
+        this.setState({ notifications: [] })
         this.props.exitModal()
         this.props.navigation.navigate(routeType, { activityID: itemID })
     }
 
     render() {
-        var {exitModal} = this.props
+        var { exitModal } = this.props
         return <View style={styles.modalContainer}>
             <NavigationEvents onWillFocus={payload => this.fetchNotifications(payload)} />
             <View style={styles.iconContainer}>
-                    <TouchableOpacity
-                        onPress={exitModal}>
-                        <View style={styles.cancelIcon}>
-                            {infoCircleIcon}
-                        </View>
-                    </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={exitModal}>
+                    <View style={styles.cancelIcon}>
+                        {infoCircleIcon}
+                    </View>
+                </TouchableOpacity>
             </View>
             <View style={styles.menuContainer}>
-            {this.state.notifications.map(({ title, id, updatedAt, routeType }, index) => {
-
-                return <NotificationLine
-                    key={id || index}
-                    id={id || index}
-                    title={title}
-                    navigationCallback={() => this.navigateTo(routeType, id)} />
-            })}
+                {this.state.notifications.map(({ title, id, updatedAt, routeType }, index) => {
+                    return <NotificationLine
+                        key={id || index}
+                        id={id || index}
+                        title={title}
+                        updatedAt={updatedAt}
+                        navigationCallback={() => this.navigateTo(routeType, id)} />
+                })}
             </View>
         </View>
     }
@@ -76,13 +78,17 @@ class NotificationOverview extends Component {
 
 export default withNavigation(NotificationOverview)
 
-const NotificationLine = ({ id, title, navigationCallback }) => {
+const NotificationLine = ({ id, title, navigationCallback, updatedAt }) => {
 
-    return (<View style={styles.menuAlternatives}>
-        <TouchableOpacity onPress={navigationCallback}>
-            <Text style={styles.menuTxt}>
-                {title}
-            </Text>
-        </TouchableOpacity>
-    </View>)
+    return (
+        <View style={styles.menuAlternatives}>
+            <TouchableOpacity
+                style={styles.notificationLink}
+                onPress={navigationCallback}>
+                <Text style={styles.menuTxt}>
+                    {title} {"\n"}<Text style={{fontWeight: "bold"}}>Updated at: </Text>{updatedAt}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
