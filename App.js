427e28a314f38761252b1f2a6f6c3066c1d270a4
Marcus Hogler <marcus@Marcuss-MacBook-Pro-Claremont.local>
2019-10-25 13:03:11 +0200

    splash screen finished for IOS

--- App.js
+++ App.js
@@ -5,10 +5,16 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import dataReducer from './src/store/reducers/dataReducer';
 import AppNavigator from './src/navigation/AppNavigator';
+import SplashScreen from 'react-native-splash-screen'
 
 const store = createStore(dataReducer);
 
 class App extends Component {
+
+  componentDidMount() {
+      SplashScreen.hide();
+  }
+  
   render() {
     return (
       <Provider store={store}>
