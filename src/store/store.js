17c9e37ff9df46dd8a169534aa7ed073f4ffb976
Tom Althin <talthin@Toms-MacBook-Pro.local>
2019-11-01 15:52:01 +0100

    removed persist storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,6 @@
 import { createStore } from 'redux';
 import { asyncStorage } from 'react-native';
-import { persistStore, persistReducer } from 'redux-persist';
 import dataReducer from './reducers/dataReducer';
-
-const persistConfig = {
-    // all data is saved to this key, if you change the key you will not find the data
-    key: 'root',
-    storage: asyncStorage,
-}
 
 const store = createStore(dataReducer);
 
